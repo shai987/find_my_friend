8d85c347a9cfa8c99e7bcff3ceba83f9cd08e623
Shai Geffen <shaigeffen33@gmail.com>
2022-12-20 12:23:53 +0200

    add comment

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ const Welcome = () => {
         return (
                 <div className="Welcome" >
                         <img src={MyImage} alt="img"></img>
-                        <img src='../images/dog_cat.jpeg' alt="img"></img>
+                        {/* <img src='../images/dog_cat.jpeg' alt="img"></img> */}
                         <article className="textttt">
                                 <p>החבר שלך אבד? תן למודל הפסיכי לעזור לך</p>
                         </article>
