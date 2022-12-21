91a0b14b2af7327280f24d9de0b0802dc21d50b8
Shai Geffen <shaigeffen33@gmail.com>
2022-12-21 12:31:13 +0200

    update files

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,10 +1,7 @@
-import MyImage from '../images/dog_cat.jpeg';
-
 const Welcome = () => {
         return (
                 <div className="Welcome" >
-                        <img src={MyImage} alt="img"></img>
-                        {/* <img src='../images/dog_cat.jpeg' alt="img"></img> */}
+                        <img src='/assets/images/dog_cat.jpeg' alt="img"></img>
                         <article className="textttt">
                                 <p>החבר שלך אבד? תן למודל הפסיכי לעזור לך</p>
                         </article>
