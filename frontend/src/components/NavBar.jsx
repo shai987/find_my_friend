674f2807304854a44400cc686a36a1fd6eb075dd
Shai Geffen <shaigeffen33@gmail.com>
2022-12-21 13:05:50 +0200

    back to defualt

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['אזור אישי', 'מי אנחנו'];
+const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const NavBar = () => {
