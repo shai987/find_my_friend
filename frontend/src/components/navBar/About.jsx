ff35461b0d9e1e0de4b1c8dde787ad3cfb737d27
adi-liel <adimaimon345@gmail.com>
2023-01-03 11:42:11 +0200

    עדכון קומפוננטת מי אנחנו
    עדכון

--- frontend/src/components/navBar/About.jsx
+++ frontend/src/components/navBar/About.jsx
@@ -1,6 +1,72 @@
+import '../../assets/css/About.css';
+
 const About = () => {
-        return (
-                <div>מי אנחנו?</div>
-        );
+    return (
+           <div dir="rtl">
+            <div className="banner"></div>
+            <div className="container"> 
+                <h1 >מי אנחנו</h1>
+                <div className="line"></div>
+            </div>  
+                 <div className="about"> 
+                        <div className="text">
+                                <h3>קצת על האתר</h3>   
+                                <p>הפרויקט נועד לסייע בהשבת בעלי חיים אבודים לבעליהם. האתר מאפשר לאדם אשר מצא או איבד כלב/חתול להעלות תמונה של החיה לאתר וכן למלא פרטים רלוונטיים. התמונות המועלות עוברות עיבוד, מזוהות לפי סוג החיה והגזע שלה ונשמרות במאגר נתונים. במידה ותימצא התאמה בין תמונה המופיעה במאגר החיות האבודות לבין תמונה שהועלתה על ידי הבעלים – תתקבל על כך התראה וניתן יהיה להשיב את החיה לבעליה. 
+מטרת הפרויקט היא לספק פלטפורמה דרכה ניתן לאתר בעל חיים אבוד ולהשיבו לבעלים באופן נוח וקל לשימוש.
+                                <img src="https://static9.depositphotos.com/1004199/1091/i/600/depositphotos_10910645-stock-photo-kitten-and-puppy-close-up.jpg" alt="cats_dogs" />
+                                </p>
+                        </div>             
+                </div>   
+            <div className="about">   
+                <div className="text">
+                    <h3>אז איך הכל התחיל</h3>  
+                    <p>אנו סטודנטים בשנה ג' בתואר למדעי המידע באוניברסיטת בר אילן. במסגרת פרויקט הגמר בו אנו לוקחים חלק, יצרנו אתר המשלב בין צד לקוח, צד שרת ומודלים. בחרנו בנושא שקרוב לליבנו, מתוך הכרה בכך שחיפוש אחר בעל חיים אבוד יכול להיות תהליך מתסכל ומייגע. אנו מאמינים כי האתר יכול לסייע באופן משמעותי באיתור בעלי חיים אבודים, ומקווים שהוא ישמש מספר רב של אנשים. 
+                    <img src="https://static8.depositphotos.com/1004199/982/i/600/depositphotos_9822329-stock-photo-cat-and-dog-together.jpg" alt="dogs_cat" />
+                    </p>
+                    <img src="https://img.freepik.com/free-vector/blue-futuristic-networking-technology_53876-97395.jpg?size=626&ext=jpg&ga=GA1.2.19967159.1672569007&semt=sph" />
+                    
+                 </div>
+
+            </div>
+             <div className="team_wrapper">
+                    <div className="team">
+                            <div className="team_member">
+                                    <div className="team_img">
+                                            <img src="https://static9.depositphotos.com/1004199/1091/i/600/depositphotos_10910645-stock-photo-kitten-and-puppy-close-up.jpg" alt="Team_image" />
+                                    </div>
+                                    <h3>אלמוג כהן</h3>
+                                    <p className="role">ממקימי האתר</p>
+                                    <p>סטודנטית שנה ג' בתואר במדעי המידע סטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידע</p>
+                            </div>
+                            <div className="team_member">
+                                    <div className="team_img">
+                                            <img src="https://ayalabarak.com/wp-content/uploads/2021/03/%D7%91%D7%9F-%D7%90%D7%93%D7%9D7-%D7%A8%D7%99%D7%91%D7%95%D7%A2%D7%99.jpg" alt="Team_image" />
+                                    </div>
+                                    <h3>רון אלי</h3>
+                                    <p className="role">ממקימי האתר</p>
+                                    <p>סטודנטית שנה ג' בתואר במדעי המידע סטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידע</p>
+                            </div>
+                            <div className="team_member">
+                                    <div className="team_img">
+                                            <img src="https://ayalabarak.com/wp-content/uploads/2021/03/%D7%91%D7%9F-%D7%90%D7%93%D7%9D7-%D7%A8%D7%99%D7%91%D7%95%D7%A2%D7%99.jpg" alt="Team_image" />
+                                    </div>
+                                    <h3>שי גפן</h3>
+                                    <p className="role">ממקימי האתר</p>
+                                    <p>סטודנטית שנה ג' בתואר במדעי המידע סטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידע</p>
+                            </div>
+                            <div className="team_member">
+                                    <div className="team_img">
+                                            <img src="https://ayalabarak.com/wp-content/uploads/2021/03/%D7%91%D7%9F-%D7%90%D7%93%D7%9D7-%D7%A8%D7%99%D7%91%D7%95%D7%A2%D7%99.jpg" alt="Team_image" />
+                                    </div>
+                                    <h3>עדי ליאל מימון</h3>
+                                    <p className="role">ממקימי האתר</p>
+                                    <p>סטודנטית שנה ג' בתואר במדעי המידע סטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידעסטודנטית שנה ג' בתואר במדעי המידע</p>
+                                    </div>
+                            </div>
+                     </div>
+            </div>
+    );
 }
 export default About;
+
+
