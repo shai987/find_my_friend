a46700663043c5a7b59269a0ef6bcf94355671fc
adi-liel <adimaimon345@gmail.com>
2023-01-04 17:59:40 +0200

    המשך עריכת קומפוננטות אודות וצור קשר
    עריכה

--- frontend/src/components/navBar/About.jsx
+++ frontend/src/components/navBar/About.jsx
@@ -24,11 +24,11 @@ const About = () => {
                 <div className="text">
                     <h3>אז איך הכל התחיל</h3>  
                     <p>
-                    <img src="https://img.freepik.com/free-vector/blue-futuristic-networking-technology_53876-97395.jpg?size=626&ext=jpg&ga=GA1.2.19967159.1672569007&semt=sph" alt="dogs_cat" />אנו סטודנטים בשנה ג' בתואר למדעי המידע באוניברסיטת בר אילן. במסגרת פרויקט הגמר בו אנו לוקחים חלק, יצרנו אתר המשלב בין צד לקוח, צד שרת ומודלים. בחרנו בנושא שקרוב לליבנו, מתוך הכרה בכך שחיפוש אחר בעל חיים אבוד יכול להיות תהליך מתסכל ומייגע. אנו מאמינים כי האתר יכול לסייע באופן משמעותי באיתור בעלי חיים אבודים, ומקווים שהוא ישמש מספר רב של אנשים. 
+                    <img src="https://images.unsplash.com/photo-1636390785299-b4df455163dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2UlMjByZWNvZ25pdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="data" />אנו סטודנטים בשנה ג' בתואר למדעי המידע באוניברסיטת בר אילן. במסגרת פרויקט הגמר בו אנו לוקחים חלק, יצרנו אתר המשלב בין צד לקוח, צד שרת ומודלים. בחרנו בנושא שקרוב לליבנו, מתוך הכרה בכך שחיפוש אחר בעל חיים אבוד יכול להיות תהליך מתסכל ומייגע. אנו מאמינים כי האתר יכול לסייע באופן משמעותי באיתור בעלי חיים אבודים, ומקווים שהוא ישמש מספר רב של אנשים. 
                     
                     </p>
                     <div className="img3">
-                    <img src="https://static8.depositphotos.com/1004199/982/i/600/depositphotos_9822329-stock-photo-cat-and-dog-together.jpg" />
+                    <img src="https://static8.depositphotos.com/1004199/982/i/600/depositphotos_9822329-stock-photo-cat-and-dog-together.jpg" alt="dog_cat "/>
                     </div>
                  </div>
             </div>
