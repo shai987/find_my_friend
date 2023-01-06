5a4ea4b0098f66518024745d4ec11f1d83e74879
Shai Geffen <shaigeffen33@gmail.com>
2023-01-06 15:19:36 +0200

    update location of the style

--- frontend/src/components/homePage/HowTheSiteWork.jsx
+++ frontend/src/components/homePage/HowTheSiteWork.jsx
@@ -5,14 +5,14 @@ const HowTheSiteWork = () => {
                 src: require('../../assets/images/petFind.png'),
                 alt: "dog_image",
                 title: "",
-                style: {
-                        height: '200px',
-                        width: '250px',
-                        marginTop: '10px',
-                        marginBottom: '100px',
-                        marginRight: '100px',
-                        marginLeft: '100px',
-                }
+                // style: {
+                //         height: '200px',
+                //         width: '250px',
+                //         marginTop: '10px',
+                //         marginBottom: '100px',
+                //         marginRight: '100px',
+                //         marginLeft: '100px',
+                // }
         }
 
         return (
@@ -23,7 +23,7 @@ const HowTheSiteWork = () => {
                                 <p>הכניסו תמונה של בעל החיים ותנו למערכת שלנו לעשות עבורכם את החיפוש.</p>
                                 <p>אנחנו נעשה הכל כדי למצוא את החבר הכי טוב שלכם כמה שיותר מהר</p>
                         </div>
-                        <div /* id='petFind' */>
+                        <div id='petFind'>
                                 <Image img={image} />
                         </div>
                 </>
