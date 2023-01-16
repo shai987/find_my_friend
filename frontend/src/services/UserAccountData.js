f37474ec799c9e093a714789bbfd5e3096016555
adi-liel <adimaimon345@gmail.com>
2023-01-16 17:23:46 +0200

    אזור אישי
    נסיונות עיצוב

---
+++ frontend/src/services/UserAccountData.js
@@ -0,0 +1,36 @@
+const requests = [
+    { type: 'כלב',
+      name: 'ניקו',
+      image: require('../assets/images/dog.jpg'),
+      race: 'רועה בלגי',
+      date: '8.1.23',
+      found_lost: 'מצאתי',
+      status: 'אותר'      
+    },
+    { type: 'חתול',
+    name: 'תולי',
+    image: require('../assets/images/catOwner.jpg'),
+    race: 'סיאמי',
+    date: '9.1.23',
+    found_lost: 'איבדתי',
+    status: 'אותר'      
+  },   
+   { type: 'כלב',
+  name: 'נלה',
+  image: require('../assets/images/catOwner.jpg'),
+  race: 'פודל',
+  date: '8.1.23',
+  found_lost: 'איבדתי',
+  status: 'טרם אותר'      
+},
+{ type: 'חתול',
+name: 'מיצי',
+image: require('../assets/images/catOwner.jpg'),
+race: 'סיאמי',
+date: '8.1.23',
+found_lost: 'איבדתי',
+status: 'אותר'      
+}
+]
+
+export default requests;
