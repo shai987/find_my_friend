f55ccd904a80189484252775574d58d9a7e2222d
Shai Geffen <shaigeffen33@gmail.com>
2023-05-02 17:57:01 +0300

    remove UNIQUE from user_password

--- backend/src/sql/db.sql
+++ backend/src/sql/db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE users_details;
 use users_details;
-CREATE TABLE users (email VARCHAR(40) PRIMARY KEY, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, user_password varchar(26) UNIQUE NOT NULL);
+-- CREATE TABLE users (email VARCHAR(40) PRIMARY KEY, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, user_password varchar(26) UNIQUE NOT NULL);
 
 -- for encryption user_password
--- CREATE TABLE users (email VARCHAR(40) PRIMARY KEY, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, user_password varchar(255) UNIQUE NOT NULL);
+CREATE TABLE users (email VARCHAR(40) PRIMARY KEY, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, user_password varchar(255) NOT NULL);
