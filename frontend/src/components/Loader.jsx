2d6254e14eeb4f110969cb278497bd4d64b0be0c
Shai Geffen <shaigeffen33@gmail.com>
2023-05-09 17:06:13 +0300

    add 3 more loaders

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,17 +1,35 @@
+import { useState, useEffect } from 'react';
+import running_dog1 from './../assets/images/dog_running_loader1.gif';
+import running_dog2 from './../assets/images/dog_running_loader2.gif';
+import running_cat1 from './../assets/images/cat_running_loader1.gif';
+import running_cat2 from './../assets/images/cat_running_loader2.gif';
 import './../assets/css/Loader.css';
-import running_dog from './../assets/images/dog_running_loader.gif';
 
-const Loader=()=> {  
-    return ( 
-           <> 
-            <div className="preloader">
-              <h2>
-                <img src={running_dog} alt="loading" />
-                בטעינה...
-              </h2>
-            </div>
-           </>    
-       ); 
+const Loader = () => {
+  const loadingArray = [running_dog1, running_dog2, running_cat1, running_cat2];
+
+  const [gifLoader, setGifLoader] = useState();
+
+  const getLoaderGif = () => {
+    const randomIndex = Math.floor(Math.random() * loadingArray.length);
+    return loadingArray[randomIndex];
+  };
+
+  useEffect(() => {
+    getLoaderGif();
+    setGifLoader(getLoaderGif());
+  }, []);
+
+  return (
+    <>
+      <div className="preloader">
+        <h2>
+          <img src={gifLoader} alt="loading" />
+          בטעינה...
+        </h2>
+      </div>
+    </>
+  );
 }
 export default Loader;
 
