7055f367ffd114d89322e075c978a244f220b121
RonEli23 <roneli386@gmail.com>
2023-05-17 12:03:44 +0300

    buttons styling

--- frontend/src/components/forms/RequestStatus.jsx
+++ frontend/src/components/forms/RequestStatus.jsx
@@ -5,24 +5,68 @@
 // update status - context
 
 // import react-router-dom
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext';
-import { useContext, useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext, useEffect } from "react";
+import * as React from 'react';
+import { Button, Container, Grid} from '@mui/material';
 
 const RequestStatus = () => {
-        const { user } = useContext(AuthContext);
-        const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-        useEffect(() => {
-                if (!user.email) {
-                        navigate('/UserStatus');
-                }
-        }, [user, navigate]);
+  useEffect(() => {
+    if (!user.email) {
+      navigate("/UserStatus");
+    }
+  }, [user, navigate]);
 
-        return (
-                <>
-                        <div>
-                                <h1>hi {user.first_name}</h1>
+  return (
+    <>
+      <Container sx={{ mt: "200px" }}>
+        <Grid container justifyContent="center" spacing={12}>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{
+                width: "200px",
+                height: "80px",
+                backgroundColor: "hsl(113, 34%, 42%)",
+                fontSize: "25px",
+              }}
+              component={Link}
+              to="/ImageForm"
+              state={{ status: "lost" }}
+            >
+              איבדתי
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{
+                width: "200px",
+                height: "80px",
+                backgroundColor: "hsl(213, 34%, 52%)",
+                fontSize: "25px",
+              }}
+              component={Link}
+              to="/ImageForm"
+              state={{ status: "found" }}
+            >
+              מצאתי
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default RequestStatus;
+
+/*
+<div>
                                 <button>
                                         <Link className='link' to='/ImageForm' state={{ status: "lost" }}>איבדתי</Link>
                                 </button>
@@ -31,9 +75,4 @@ const RequestStatus = () => {
                                 <button>
                                         <Link className='link' to='/ImageForm' state={{ status: "found" }}>מצאתי</Link>
                                 </button>
-                        </div>
-                </>
-        );
-}
-
-export default RequestStatus;
+                        </div>*/
