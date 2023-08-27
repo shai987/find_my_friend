20982a5f5c65af76fcd83ba4b12fbcdda9b644a0
Shai Geffen <shaigeffen33@gmail.com>
2023-08-27 10:34:42 +0300

    hide red container

--- frontend/src/components/forms/ImageForm.jsx
+++ frontend/src/components/forms/ImageForm.jsx
@@ -121,7 +121,7 @@ const ImageForm = () => {
                                         <br></br>
                                         <Button variant="contained" type='submit' onClick={handleSubmit}>שלח</Button>
                                         <br></br>
-                                        <p className="response">{errMassage}</p>
+                                        {errMassage && <p className="response">{errMassage}</p>}
                                 </form>
                                         : <PetDetails pet_type={pet_type} pet_breeds={pet_breeds} />)
                         }
