97ffd820b25ce288de8784680f84ccc310a6502d
Shai Geffen <shaigeffen33@gmail.com>
2023-08-30 20:47:52 +0300

    Add MostFoundPets route

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { handlePetImage, handlePetDetails } from '../controllers/pet.controller.js';
+import { handlePetImage, handlePetDetails, handleMostFoundPets } from '../controllers/pet.controller.js';
 import { handleSignUp, handleSignIn, handleDeleteAllUser, handleGetAllUsers, handleContactUser, handleUserInfo, handleDeleteUser, handleContactUs } from '../controllers/user.controller.js';
 import { validate } from "../middlewares/validator.middleware.js";
 
 router.post("/uploadImage", handlePetImage);
 
 router.post("/petDetails", validate('handlePetDetails'), handlePetDetails);
+
+router.get("/MostFoundPets", handleMostFoundPets);
 
 router.post("/userSignUp", validate('handleSignUp'), handleSignUp);
 
