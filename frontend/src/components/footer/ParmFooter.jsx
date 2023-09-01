f87f7bdcaa756ca2a3d4ccf0033b3c4c0b0b4af7
Shai Geffen <shaigeffen33@gmail.com>
2023-09-01 13:05:13 +0300

    Deleting irrelevant lines

--- frontend/src/components/footer/ParmFooter.jsx
+++ frontend/src/components/footer/ParmFooter.jsx
@@ -1,20 +1,11 @@
-// import libraries from material-ui
-import Typography from '@mui/material/Typography';
-// import icons from icons-material
-import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import MarkEmailReadTwoToneIcon from '@mui/icons-material/MarkEmailReadTwoTone';
-import ContactPageIcon from '@mui/icons-material/ContactPage';
+// import react-router-dom
+import { Link } from 'react-router-dom';
 // import css
 import '../../assets/css/ParmFooter.css';
 //import image
 import pen from '../../assets/images/pen.png';
 import email from '../../assets/images/email.png';
 import social_media from '../../assets/images/social_media.png';
-import { color } from '@mui/system';
-import { Link } from 'react-router-dom';
 
 const ParmFooter = () => {
         return (
@@ -51,49 +42,6 @@ const ParmFooter = () => {
                                 Copyright ©  Find My Friend{' ' + new Date().getFullYear()}
                         </span>
                 </>
-
-                // <>         
-                //         {/* About */}
-                //         <Typography className='he' variant="body1">
-                //                 {'מוזמנים לכתוב לנו כאן לכל שאלה'}
-                //                 <Link className='try' href="/ContactUs" underline="none" rel="noopener noreferrer">
-                //                         <ContactPageIcon color="primary" fontSize="small" />
-                //                         {/* <img src={require('../assets/images/pen.png')} alt="pen"></img> */}
-                //                 </Link>
-                //         </Typography>
-
-                //         {/* Contact-Mail */}
-                //         <Typography className='he' variant="body1">
-                //                 {'נשמח לשמוע מכם '}
-                //                 <Link color="inherit" href="mailto:findmyfriend10@gmail.com">
-                //                         <MarkEmailReadTwoToneIcon color="primary" fontSize="small" />
-                //                 </Link>
-                //         </Typography>
-
-                //         {/* Social Media */}
-                //         <Typography className='he' variant="body1" >
-                //                 {'חפשו אותנו ברשתות החברתיות '}
-                //                 <Link color="inherit" href="">
-                //                         <FacebookSharpIcon sx={{ color: 'blue' }} fontSize="small" />
-                //                         <WhatsAppIcon color="success" fontSize="small" />
-                //                         <InstagramIcon sx={{ color: '#C13584' }} fontSize="small" />
-                //                         <TwitterIcon color="primary" fontSize="small" />
-                //                 </Link>
-                //         </Typography>
-
-                //         {/* Copyright  */}
-                //         <Typography className='copyright' variant="body1">
-                //                 <span>
-
-
-                //                 {'Copyright © '}
-                //                 <Link href="https://github.com/shai987/find_my_friend" target="_blank" rel="noopener noreferrer" underline="none">
-                //                         <b className='bold'>Find My Friend</b>
-                //                 </Link>
-                //                 {' ' + new Date().getFullYear()}
-                //                  </span>
-                //         </Typography>
-                // </>
         );
 }
 
