9f6af1ffb9dfd975bcc8b19389518b61b9268606
Shai Geffen <shaigeffen33@gmail.com>
2023-09-01 14:06:28 +0300

    Deleting irrelevant lines + change imports order

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
-import CssBaseline from '@mui/material/CssBaseline';
+// import libraries from material-ui
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-
+import CssBaseline from '@mui/material/CssBaseline';
+// import our components
 import ParmFooter from './ParmFooter';
 import ScrollToTop from "./ScrollToTop";
 
@@ -9,16 +10,14 @@ const Footer = () => {
         return (
                 <>
                         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', mt: '-400px' }}>
-
                                 <CssBaseline />
-
                                 <Box component="footer"
                                         sx={{
                                                 py: 1,
                                                 px: 2,
                                                 mt: 'auto',
                                                 backgroundColor: '#333333'
-                                                
+
                                         }}
                                 >
                                         <Container maxWidth="sm">
