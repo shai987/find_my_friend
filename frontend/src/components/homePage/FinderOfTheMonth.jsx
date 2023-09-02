419bf667476f623727c0b76019c03ee21c8e78b3
Shai Geffen <shaigeffen33@gmail.com>
2023-09-02 19:04:03 +0300

    Update userWithMostFoundPets + change imports order

--- frontend/src/components/homePage/FinderOfTheMonth.jsx
+++ frontend/src/components/homePage/FinderOfTheMonth.jsx
@@ -1,8 +1,11 @@
+// import libraries from react
 import { useEffect, useState } from "react";
-import axios from 'axios';
-import '../../assets/css/FinderOfTheMonth.css';
+// import libraries from material-ui
 import Avatar from "@mui/material/Avatar";
-
+// import css
+import '../../assets/css/FinderOfTheMonth.css';
+// import axios
+import axios from 'axios';
 axios.defaults.baseURL = 'http://127.0.0.1:8080/route';
 
 const FinderOfTheMonth = () => {
@@ -13,7 +16,6 @@ const FinderOfTheMonth = () => {
                 try {
                         const response = await axios.get("/MostFoundPets");
                         setUserWithMostFoundPets(response.data);
-                        console.log(userWithMostFoundPets)
                 } catch (err) {
                         setErrMassage(err.message);
                         console.log(err);
@@ -29,25 +31,25 @@ const FinderOfTheMonth = () => {
                         <h2 className="h2Class">מוצא החודש🏆</h2>
                         {userWithMostFoundPets ? (
                                 <article>
-                                        {userWithMostFoundPets?.message==="אף משתשמש לא מצא חיות" ?(
+                                        {userWithMostFoundPets?.message === "אף משתשמש לא מצא חיות" ? (
                                                 <section>
-                                                         < Avatar alt="no one" title="no one" 
-                                                sx={{ width: '150px', height: '150px', my: -1.5, backgroundColor: '#FF8A00', border: '2px solid #fff', mr: 'auto', ml: 'auto' }}/>
-                                                <br/>
-                                                <p>אוף! עדיין לא מצאו מישהו החודש</p>        
+                                                        < Avatar alt="no one" title="no one"
+                                                                sx={{ width: '150px', height: '150px', my: -1.5, backgroundColor: '#FF8A00', border: '2px solid #fff', mr: 'auto', ml: 'auto' }} />
+                                                        <br />
+                                                        <p>אוף! עדיין לא מצאו מישהו החודש</p>
                                                 </section>
-                                                                               ):
-                                        <section>
-                                                < Avatar alt="d"
-                                        sx={{ width: '150px', height: '150px', my: -1.5, backgroundColor: '#FF8A00', border: '2px solid #fff', mr: 'auto', ml: 'auto' }}
-                                        src={`https://anonymous-animals.azurewebsites.net/avatar/:${userWithMostFoundPets.userEmail}`} />
-                                        <br />
-                                        {userWithMostFoundPets.userEmail} מצא הכי הרבה חיות (
-                                                {userWithMostFoundPets.foundPetsCount} חיות נמצאו).
-                                        </section>
-                                        
+                                        ) :
+                                                <section>
+                                                        < Avatar alt="d"
+                                                                sx={{ width: '150px', height: '150px', my: -1.5, backgroundColor: '#FF8A00', border: '2px solid #fff', mr: 'auto', ml: 'auto' }}
+                                                                src={`https://anonymous-animals.azurewebsites.net/avatar/:${userWithMostFoundPets.userEmail}`} />
+                                                        <br />
+                                                        {userWithMostFoundPets.userName} מצא הכי הרבה חיות (
+                                                        {userWithMostFoundPets.foundPetsCount} חיות נמצאו).
+                                                </section>
+
                                         }
-                                                                              
+
                                 </article>
                         ) : (
                                 <p>טוען...</p>
